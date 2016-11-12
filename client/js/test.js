0f080a39c7f9000ff6fff07618e7a1aa25bce1d2
Austin Bodzas <austinbodzas@gmail.com>
2016-11-11 23:58:18 -0500

    added functionality for parsing packets from binary to float arrays

--- client/js/test.js
+++ client/js/test.js
@@ -1,21 +1,35 @@
 var SerialPort = require('serialport');
-var port = new SerialPort('/dev/ttyACM0', {
+var sPort = new SerialPort('/dev/ttyACM0', {
 	baudrate: 9600,
 	parser: SerialPort.parsers.raw
 });
 
-port.on("open", function () {
+sPort.on("open", function () {
   console.log('open');
-  port.on('data', function(data) {
-      var smaller = data.slice(0,60);
-
-      var floatarr = new Float32Array(1);
-
-      floatarr[0] |= (data[0] << 24);
-      floatarr[0] |= (data[1] << 16);
-      floatarr[0] |= (data[2] << 8);
-      floatarr[0] |= data[3];
-
-      console.log(floatarr[0].toString());
+  sPort.on('data', function(data) {
+      var a = packetToFloatArr(data);
+      console.log(a[8]);
   });
 });
+
+/**
+ * Creates an array of 15 floats from binary buffer of length 60+
+ */
+const packetToFloatArr = (byteData)  => {
+    buffer = byteData.slice(0, 60);
+
+    var lower = 0;
+    var upper = 4;
+
+    var floatArr = new Float32Array(15);
+
+    for (var i = 0; i < 15; i++) {
+        var tmpSlice = buffer.slice(lower,upper);
+        floatArr[i] = tmpSlice.readFloatLE();
+
+        lower += 4;
+        upper += 4;
+    }
+
+    return floatArr;
+};
